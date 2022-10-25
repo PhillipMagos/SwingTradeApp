4ed96ba0546dc708fd046c465db032604cf05198
phillip.magos <phillipmagos@gmail.com>
2022-10-25 18:27:58 -0500

    FE start up

--- index.js
+++ index.js
@@ -7,5 +7,17 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.send('/home')
 
+app.get('/', (req, res) => {
+  res.redirect('/home');
+})
+app.get('/about', (req, res) => {
+  res.redirect('/about');
+})
+app.get('/contact', (req, res) => {
+  res.redirect('/contact');
+})
+
+
+
   const port = process.env.PORT || 3000
 app.listen(port, ()=> console.log(`wbe host on port ${port}`))
